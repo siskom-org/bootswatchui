17ed2a6db372ee603e67ef2fef38a164f30cf5a7
Reza Nurfachmi <aaezha@gmail.com>
2023-02-09 11:03:13 +0700

    feat: vite x bootswatch 🔥

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                "resources/css/bootstrap.min.css",
+                "resources/css/app.css",
+                "resources/js/app.js",
+            ],
             refresh: true,
         }),
     ],
