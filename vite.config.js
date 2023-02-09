7084281a7aa60b057f29931d9c335ada5c174268
Reza Nurfachmi <aaezha@gmail.com>
2023-02-09 11:25:51 +0700

    fix: vite assets 🐒

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                "resources/css/bootstrap.min.css",
                 "resources/css/app.css",
                 "resources/js/app.js",
             ],
