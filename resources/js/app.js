58f8669f05a1021c3579c743294fac7f6482d2e9
Reza Nurfachmi <aaezha@gmail.com>
2023-02-10 09:33:18 +0700

    feat: components on home 🍟

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,19 @@
 import './bootstrap';
 import './bootstrap.bundle.min';
 import './prism';
+
+// enable popovers
+const popoverTriggerList = document.querySelectorAll(
+    '[data-bs-toggle="popover"]'
+);
+const popoverList = [...popoverTriggerList].map(
+    (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+);
+
+// enable tooltips
+const tooltipTriggerList = document.querySelectorAll(
+    '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+    (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
